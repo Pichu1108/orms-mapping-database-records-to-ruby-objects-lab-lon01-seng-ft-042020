6ae9ac09a24f418f41d7c6caa42195deaab40b83
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-05-02 18:12:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE students.grade = ?
+      WHERE students.grade = 9
     SQL
 
     DB[:conn].execute(sql).map do |row|
